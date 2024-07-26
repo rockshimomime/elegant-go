3c4fc075439709d7f678c95b04271415c11e5a83
alef <sh.alencar@rocketmail.com>
2024-07-26 16:40:58 -0300

    feat: create variables to show its default values

---
+++ 2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		a string
+		b bool
+		c int32
+		d byte
+		e float32
+	)
+
+
+	fmt.Printf("A: %s, B: %t C: %d D: %c E: %f", a, b, c, d, e)
+
+}
